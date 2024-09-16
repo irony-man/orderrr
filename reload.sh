3a7924dfaa29d3ab8e878393b2fffed71893fac9
irony-man <shivamrai0401@gmail.com>
2024-09-16 09:30:31 +0530

    build file changed

--- reload.sh
+++ reload.sh
@@ -6,6 +6,10 @@ rm -rf common/migrations
 rm -rf sql_lite.db
 ./manage.py makemigrations common
 ./manage.py migrate
+cd common/static_dev/common
+npm i --legacy-peer-deps
+npm run build
+cd ../../../
 ./manage.py collectstatic --no-input
 ./manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('shivam', '', '123')" | python manage.py shell
