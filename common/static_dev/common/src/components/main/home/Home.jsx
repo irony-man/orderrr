659edcbdc531c74c1cafef6fa18b430e5e32e9fa
irony-man <shivamrai0401@gmail.com>
2024-12-23 04:29:39 +0530

    Old version link added

--- common/static_dev/common/src/components/main/home/Home.jsx
+++ common/static_dev/common/src/components/main/home/Home.jsx
@@ -14,13 +14,24 @@ const Home = () => {
           <Typography variant="h2" className="fw-normal mb-2">
             Welcome to <strong>Orderrr</strong>
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" className="mb-4">
             Are you a designer with a passion for turning ideas into stunning
             visuals? <strong>Orderrr</strong> is the perfect platform for you
             to showcase and sell your unique designs to a global audience.
             Whether you specialize in graphics, illustrations, templates, or
             any other creative work, our marketplace is designed to help you
             reach potential buyers and turn your creative talents into profit.
+          </Typography>
+          <Typography variant="body1">
+            Switch to
+            <a
+              className="ms-1"
+              href="https://orderrr.onrender.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+            Old Version
+            </a>
           </Typography>
         </Box>
       </div>
